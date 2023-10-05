b16c6f21ff5a21f306d0ed1bbcdff7fa38c00361
Alexander Mennborg <alexander.mennborg@gmail.com>
2023-10-05 13:11:49 +0200

    Remove garbage files

--- src/api.js
+++
@@ -1,9 +0,0 @@
-import { Amplify, API, graphqlOperation } from 'aws-amplify';
-import { createTodoItem, updateTodoItem, deleteTodoItem } from './graphql/mutations.js';
-import awsconfig from './aws-exports.js';
-Amplify.configure(awsconfig);
-
-const todo = { text: "Hello world!", done: false };
-
-/* create a todo */
-await API.graphql(graphqlOperation(createTodoItem, {input: todo}));
